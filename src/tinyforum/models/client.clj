4001fb51268e583c7177c0bbfac31238b12c9b3f
Kyle Travis <wetscience@gmail.com>
2013-08-24 14:52:03 -0700

    x

--- src/tinyforum/models/client.clj
+++ src/tinyforum/models/client.clj
@@ -2,5 +2,5 @@
      (:use [aleph.redis :only (redis-client)]))
 
 (def client (delay (redis-client 
-                     {:host "pub-redis-10331.us-east-1-4.3.ec2.garantiadata.com" 
-                      :port 10331 :password "VRSidx9WYg7QYvUe"})))
+                     {:host "pub-redis-13618.us-east-1-4.3.ec2.garantiadata.com" 
+                      :port 13618 :password "NLwnDlCIziUk3H0U"})))
