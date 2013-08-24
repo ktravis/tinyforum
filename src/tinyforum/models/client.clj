1511b171bf0d6798e469a6d12131f4bfa2805b48
Kyle Travis <wetscience@gmail.com>
2013-08-24 15:20:06 -0700

    switched to redis to go

--- src/tinyforum/models/client.clj
+++ src/tinyforum/models/client.clj
@@ -2,5 +2,5 @@
      (:use [aleph.redis :only (redis-client)]))
 
 (def client (delay (redis-client 
-                     {:host "pub-redis-12925.us-east-1-4.3.ec2.garantiadata.com" 
-                      :port 12925 :password "8PqiNd6uoNsVDYRH"})))
+                     {:host "scat.redistogo.com" 
+                      :port 10023 :password "c5de129266f4e611d5a6e4d6b07504b2"})))
