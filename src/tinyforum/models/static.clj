6cfe64847577ec630b360aa0eadf4285446a4486
Kyle Travis <wetscience@gmail.com>
2013-08-24 16:00:38 -0700

    this should be all

--- src/tinyforum/models/static.clj
+++ src/tinyforum/models/static.clj
@@ -3,31 +3,31 @@
 
 
 (defn set-faq! [new-faq]
-  (@r [:set "FAQ" new-faq]))
+  @(r [:set "FAQ" new-faq]))
 
 (defn get-faq []
-  (@r [:get "FAQ"]))
+  @(r [:get "FAQ"]))
 
 (defn set-masthead! [new-masthead]
-  (@r [:set "MASTHEAD" new-masthead]))
+  @(r [:set "MASTHEAD" new-masthead]))
 
 (defn get-masthead []
-  (@r [:get "MASTHEAD"]))
+  @(r [:get "MASTHEAD"]))
 
 (defn set-banner! [new-banner]
-  (@r [:set "BANNER" new-banner]))
+  @(r [:set "BANNER" new-banner]))
 
 (defn get-banner []
-  (@r [:get "BANNER"]))
+  @(r [:get "BANNER"]))
 
 (defn set-lead! [new-lead]
-  (@r [:set "LEAD" new-lead]))
+  @(r [:set "LEAD" new-lead]))
 
 (defn get-lead []
-  (@r [:get "LEAD"]))
+  @(r [:get "LEAD"]))
 
 (defn set-footer! [new-footer]
-  (@r [:set "FOOTER" new-footer]))
+  @(r [:set "FOOTER" new-footer]))
 
 (defn get-footer []
-  (@r [:get "FOOTER"]))
+  @(r [:get "FOOTER"]))
--- src/tinyforum/models/users.clj
+++ src/tinyforum/models/users.clj
@@ -11,46 +11,46 @@
 
 
 (defn is-admin? [u] (or (contains? admin u)
-                        (not= 0 (@r [:sismember "admins" u])))) 
+                        (not= 0 @(r [:sismember "admins" u])))) 
 
 (defn user-get [email]
-  (let [user (apply hash-map (@r [:hgetall (key-user email)]))]
+  (let [user (apply hash-map @(r [:hgetall (key-user email)]))]
     (when (not (empty? user))
       (merge {:email (user "email") :pass (user "pass")}
              {:topics (sort-maps-by (map topic-get
-                                         (@r [:smembers (key-user-topics email)]))
+                                         @(r [:smembers (key-user-topics email)]))
                                     :title)
               :comments (sort-maps-by (map comment-get
-                                           (@r [:smembers (key-user-comments email)]))
+                                           @(r [:smembers (key-user-comments email)]))
                                       :body)}))))
 
 (defn user-set-email! [email new-email]
-  (@r [:lrem "emails" 0 email])
-  (@r [:lpush "emails" new-email])
-  (@r [:hset (key-user email) "email" new-email]))
+  @(r [:lrem "emails" 0 email])
+  @(r [:lpush "emails" new-email])
+  @(r [:hset (key-user email) "email" new-email]))
 
 (defn users-get-all []
-  (@r [:lrange "emails" 0 (dec (@r [:llen "emails"]))])
+  @(r [:lrange "emails" 0 (dec @(r [:llen "emails"]))])
   )
 
 (defn user-set-pass! [email new-pass]
-  (@r [:hset (key-user email) "pass" (crypt/encrypt new-pass)]))
+  @(r [:hset (key-user email) "pass" (crypt/encrypt new-pass)]))
 
 (defn user-add-topic! [email topic-id]
-  (@r [:sadd (key-user-topics email) topic-id]))
+  @(r [:sadd (key-user-topics email) topic-id]))
 
 (defn user-add-comment! [email comment-id]
-  (@r [:sadd (key-user-comments email) comment-id]))
+  @(r [:sadd (key-user-comments email) comment-id]))
 
 (defn user-remove-topic! [email topic-id]
-  (@r [:srem (key-user-topics email) topic-id]))
+  @(r [:srem (key-user-topics email) topic-id]))
 
 (defn user-remove-comment! [email comment-id]
-  (@r [:srem (key-user-comments email) comment-id]))
+  @(r [:srem (key-user-comments email) comment-id]))
 
 (defn user-promote! [email]
-  (@r [:sadd "admins" email]))
+  @(r [:sadd "admins" email]))
 
 (defn user-demote! [email]
-  (@r [:srem "admins" email]))
+  @(r [:srem "admins" email]))
 
