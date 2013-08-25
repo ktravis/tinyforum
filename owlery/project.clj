4842ba6748e905f9a50f0fdf46008f4be1633767
Kyle Travis <wetscience@gmail.com>
2013-08-25 15:40:23 -0700

    removing excess

--- owlery/project.clj
+++
@@ -1,8 +0,0 @@
-(defproject owlery "0.1.0-SNAPSHOT"
-            :description "FIXME: write this!"
-            :dependencies [[org.clojure/clojure "1.4.0"]
-                           [noir "1.3.0-beta3"]
-                           [markdown-clj "0.9.29"]
-                           [aleph "0.3.0-rc2"]]
-            :main owlery.server)
-
