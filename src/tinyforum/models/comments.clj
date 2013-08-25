e34af5c94ed4316f9a6cafbf9378f079fc234d2f
Kyle Travis <wetscience@gmail.com>
2013-08-24 18:34:43 -0700

    b

--- src/tinyforum/models/comments.clj
+++ src/tinyforum/models/comments.clj
@@ -4,7 +4,7 @@
      (:use tinyforum.models.client))
 
 (defn comment-get [id]
-  (let [comm (apply hash-map @(r [:hgetall (key-comment id)]))]
+  (let [comm (apply hash-map @(@r [:hgetall (key-comment id)]))]
     (when (not (empty? comm))
       {:post-time (comm "post-time")
        :author (comm "author")
@@ -14,27 +14,27 @@
        })))
 
 (defn comment-set-id! [id new-id]
-  @(r [:hset (key-comment id) "id" new-id]))
+  @(@r [:hset (key-comment id) "id" new-id]))
 
 (defn comment-set-parent! [id new-parent]
-  @(r [:hset (key-comment id) "parent" new-parent]))
+  @(@r [:hset (key-comment id) "parent" new-parent]))
 
 (defn comment-set-post-time! [id new-post-time]
-  @(r [:hset (key-comment id) "post-time" new-post-time]))
+  @(@r [:hset (key-comment id) "post-time" new-post-time]))
 
 (defn comment-set-author! [id new-author]
-  @(r [:hset (key-comment id) "author" new-author]))
+  @(@r [:hset (key-comment id) "author" new-author]))
 
 (defn comment-set-body! [id new-body]
-  @(r [:hset (key-comment id) "body" new-body]))
+  @(@r [:hset (key-comment id) "body" new-body]))
 
 (defn cids-get-latest []
-  (parse-int @(r [:lindex "cids" 0])))
+  (parse-int @(@r [:lindex "cids" 0])))
 
 
 (defn store-raw-comment [comm]
   (let [id (comm :id)]
-    @(r [:lpush "cids" id])
+    @(@r [:lpush "cids" id])
     (comment-set-id! id id)
     (comment-set-parent! id (comm :parent))
     (comment-set-post-time! id (get-time))
@@ -45,7 +45,7 @@
   (dorun (map store-raw-comment comments)))
 
 (defn comments-length []
-  @(r [:llen "cids"]))
+  @(@r [:llen "cids"]))
 
 (defn is-comment-author? [cid u]
   (if (= (:author (comment-get cid)) u) true nil))
