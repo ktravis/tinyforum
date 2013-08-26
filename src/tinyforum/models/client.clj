19da6d945b0ec5fd154d4ea5ee300bf793f7c56e
Kyle Travis <wetscience@gmail.com>
2013-08-25 17:27:53 -0700

    settings

--- src/tinyforum/models/client.clj
+++ src/tinyforum/models/client.clj
@@ -1,9 +1,9 @@
 (ns tinyforum.models.client
      (:use [aleph.redis :only (redis-client)]))
 
-(def redis-host "localhost")
-(def redis-password "")
-(def redis-port 6379)
+(def redis-host "scat.redistogo.com")
+(def redis-password "db52dcc762988a408ecd823ddaec2395")
+(def redis-port 10034)
 
 (def r (delay (redis-client 
   {:host redis-host
